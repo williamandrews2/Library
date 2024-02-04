6e77b63ff21de86c50aae73ff5291047160e91a1
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-02-04 15:38:03 -0600

    Displayed all books in the HTML.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Library
+
+## Introduction
+
+This is a simple Library app that will take user input to add books to their library. There is no database connected to this project as of right now.
+
+## To-Do:
+
+- Mobile optimization
+- UI
+
+## Known Issues:
+
+- None at this time.
--- script.js
+++ script.js
@@ -1,20 +1,60 @@
+const myLibrary = [];
+const content = document.querySelector(".content");
+
+// Constructor for Book objects
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read; //Unsure if this is how they want it to be. Should it be a boolean?
-
-  this.info = function () {
-    return (
-      this.title +
-      " by " +
-      this.author +
-      ", " +
-      this.pages +
-      " pages, " +
-      this.read
-    );
-  };
+  this.read = read;
 }
 
-const firstBook = new Book("Harry Potter", "J.K. Rowling", 234, "not read yet");
+// Create a new book and add it to the myLibrary array
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+
+  // Check if newBook is already in the library
+  const found = myLibrary.some((book) => book.title === newBook.title);
+  if (!found) {
+    myLibrary.push(newBook);
+  } else {
+    alert("This book is already in your Library!");
+  }
+}
+
+// Display each book on the page
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    //Create new card element for the book
+    const card = document.createElement("div");
+    card.className = "card";
+
+    // Information displayed in each card
+    const title = document.createElement("h1");
+    title.className = "title";
+
+    const author = document.createElement("h3");
+    author.className = "author";
+
+    const info = document.createElement("p");
+    info.className = "info";
+
+    title.innerHTML = `${myLibrary[i].title}`;
+    author.innerHTML = `${myLibrary[i].author}`;
+    info.innerHTML = `${myLibrary[i].title}, by ${myLibrary[i].author}, ${myLibrary[i].pages} pages, ${myLibrary[i].read}`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(info);
+
+    content.appendChild(card);
+  }
+}
+
+// ------------ TESTING SECTION-------------
+
+addBookToLibrary("Book1", "Jack", "234", "not read");
+addBookToLibrary("Life Book 2", "Kanye West", "121", "read");
+addBookToLibrary("Rose from Concrete", "Lebron James", "98", "not read");
+
+displayBooks();
